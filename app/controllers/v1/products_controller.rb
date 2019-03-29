c46171a0289680f676108a416bf5a3723a11eabe
muhammadawais <awaisshaikh94@gmail.com>
2019-03-29 15:03:18 +0500

    Action Show done for products

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -3,4 +3,10 @@ class V1::ProductsController < ApplicationController
         product = Product.all
         render json: { product: product }, status: :ok
     end
+
+    def show
+        product = Product.find(params[:id])
+        render json: product, product: :ok
+    end
+    
   end
