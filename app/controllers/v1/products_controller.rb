302132c0863baf377048f2ee96900ff906ca89e8
muhammadawais <awaisshaikh94@gmail.com>
2019-03-29 15:07:31 +0500

    create and update methods done in products controller

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -4,9 +4,33 @@ class V1::ProductsController < ApplicationController
         render json: { product: product }, status: :ok
     end
 
+    def create
+        product = Product.new(product_params)
+        if product.save
+          render json: product, status: :created
+        else
+          render json: { errors: product.errors }, status:    :unprocessable_entity
+    end
+        
+
     def show
         product = Product.find(params[:id])
         render json: product, product: :ok
+    end
+
+    def update
+        product = Product.find(params[:id])
+        if product.update(product_params)
+          render json: product, status: :ok
+        else
+          render json: { errors: product.errors }, status: :unprocessable_entity
+        end
+    end
+
+    private
+        
+    def product_params
+        params.require(:product).permit(:name, :price, :description, :category_id, :stock)
     end
     
   end
